aade47eee887c7085313e8e914cf66e0a8c0be12
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2024-10-17 00:17:46 +0900

    style: rubocopを適用しました

--- rails/app/controllers/concerns/devise_hack_fake_session.rb
+++ rails/app/controllers/concerns/devise_hack_fake_session.rb
@@ -14,12 +14,11 @@ module DeviseHackFakeSession
     before_action :set_fake_session
 
     private
-    def set_fake_session
-      if Rails.configuration.respond_to?(:api_only) && Rails.configuration.api_only
-        request.env["rack.session"] ||= ::DeviseHackFakeSession::FakeSession.new
+
+      def set_fake_session
+        if Rails.configuration.respond_to?(:api_only) && Rails.configuration.api_only
+          request.env["rack.session"] ||= ::DeviseHackFakeSession::FakeSession.new
+        end
       end
-    end
-
   end
-
 end
